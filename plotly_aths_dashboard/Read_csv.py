13a443527f2c00a178804d2e267e5a1413a65eca
Darcy Roper <d.roper@outlook.com>
2023-03-24 09:30:47 +1000

    Pathways updated

--- plotly_aths_dashboard/Read_csv.py
+++ plotly_aths_dashboard/Read_csv.py
@@ -5,6 +5,18 @@ import pandas as pd
 df = pd.read_csv('somethingNew.csv', sep='|')
 
 dff = df[df['id']==14550669]
-dff['Count'] = dff.groupby(['country'])['__typename'].transform('count')
+#dff['Count'] = dff.groupby(['country'])['__typename'].transform('count')
+
+# convert date in dff to dattimeseries
+dff['date'] = pd.to_datetime(df['date'])
+#season_best = dff.set_index(['date']).groupby(pd.Grouper(freq='A'))['resultscore'].nlargest(3)
+
+# add column for season
+dff['season'] = dff['date'].dt.strftime('%Y')
+dff['season'] = dff['season'].astype(int)
+# add column for season best result
+dff['sn_best'] = dff.groupby(['season', 'disciplineCode'])['resultscore'].transform('max')
 
 print(dff)
+
+print(dff.dtypes)
--- plotly_aths_dashboard/Trial_app.py
+++ plotly_aths_dashboard/Trial_app.py
@@ -13,12 +13,14 @@ df["date"] = pd.to_datetime(df["date"], infer_datetime_format=True)
 
 # Build your components
 app = Dash(__name__, external_stylesheets=[dbc.themes.QUARTZ])
-mytitle = dcc.Markdown(children='# Results')
+mytitle = dcc.Markdown(children='# Australian Athletics Analytics')
 mygraph = dcc.Graph(id='mygraph', figure={})
 mapgraph = dcc.Graph(id='mapgraph', figure={})
+sngraph = dcc.Graph(id='sngraph', figure={})
 dropdown = dcc.Dropdown(options=df['id'].unique(),
                         value='14550669',  # initial value displayed when page first loads
-                        clearable=False, style={'color': 'Black'})
+                        clearable=False, style={'color': 'Black'},
+                        searchable=True)
 
 # Customize Layout
 app.layout = dbc.Container([
@@ -30,11 +32,15 @@ app.layout = dbc.Container([
     ], justify='center'),
     dbc.Row([
         dbc.Col([mygraph], width=6),
-        dbc.Col([mapgraph], width=6)
+        dbc.Col([sngraph], width=6)
     ]),
+    dbc.Row([
+        dbc.Col([mapgraph], width=6)
+    ], justify='left'),
 
 ], fluid=True)
 
+##########################################################
 # Callback allows components to interact
 @app.callback(
     Output(component_id='mygraph', component_property='figure'),
@@ -43,12 +49,31 @@ app.layout = dbc.Container([
 def update_graph(user_input):  # function arguments come from the component property of the Input
     print(user_input)
     #print(df[df['id']==user_input])
-    fig = px.scatter(df[df['id']==user_input], x="date", y="resultscore", color="discipline", hover_data=["category", "place", "mark", "wind"])
-
+    fig = px.scatter(df[df['id']==user_input], x="date", y="resultscore",
+                     color="disciplineCode", hover_data=["category", "place", "mark", "wind"],
+                     title="Event Results - Hover for more data!")
+    fig.update_layout(legend_title_text='')
     return fig  # returned objects are assigned to the component property of the Output
 
-##########################################################
-# 2nd Callback for map
+
+@app.callback(
+    Output(component_id='sngraph', component_property='figure'),
+    Input(dropdown, component_property='value')
+)
+def update_sngraph(user_input):
+    dfb = df[df['id']==user_input]  # data we're working with is all rows related to 'user_input'
+    # add column for season
+    dfb['season'] = dfb['date'].dt.strftime('%Y')
+    dfb['season'] = dfb['season'].astype(int)  # change the data type so x axis is continuous
+    # add column for season best result
+    dfb['sn_best'] = dfb.groupby(['season', 'disciplineCode'])['resultscore'].transform('max')
+    fig3 = px.line(dfb, x="season", y="sn_best", color="disciplineCode", markers=True,
+                   title="Season's bests")
+    fig3.update_layout(legend_title_text='')
+
+    return fig3
+
+
 @app.callback(
     Output(component_id='mapgraph', component_property='figure'),
     Input(dropdown, component_property='value')
@@ -58,11 +83,13 @@ def update_mapgraph(user_input):  # function arguments come from the component p
     dff = df[df['id']==user_input]
     dff['count'] = dff.groupby(['country'])['__typename'].transform('count')
     fig2 = px.scatter_geo(dff, locations="country", color="country",
-                          size='count', projection="orthographic")
+                          size='count', projection="orthographic",
+                          title='Competition Travel Footprint')
     fig2.update_traces(marker=dict(line=dict(width=2, color='Blue')), selector=dict(mode='markers'))
     fig2.update_traces(marker_sizemin=5, selector=dict(mode='markers'))
 
     return fig2  # returned objects are assigned to the component property of the Output
+
 
 
 # Run app
